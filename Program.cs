f34278aa4f2143be13456a8bce46afd144e2f6ec
Stanley Melgar <stanlydoge@gmail.com>
2025-04-12 20:52:43 -0600

    `tutoria 12:` ejercicio while anidado

--- Program.cs
+++ Program.cs
@@ -6,60 +6,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        bool isCurrentlyWorking = true;
-        int totalQuantity = 0;
-        
-        Console.WriteLine("--- Gestion de compra de toallas ---");
-        Console.WriteLine("Para evitar el cobro por envio, haga una compra igual o mayor a 3 sets");
-        Console.WriteLine("----------------------------------");
-        Console.ReadKey();
-        Console.Clear();
-        
-        
-        // Inicio del dia laboral
-        while (isCurrentlyWorking)
+        const double SALARY_BASE = 182.50; //365.00
+        const int BONUS = 100;
+        const double COMISION = 0.03; //3%
+        bool biweekly = true;
+
+        // Quincena iniciada
+        // Registro de n vendedores
+        while (biweekly) //boolean - bandera
         {
+            bool registryAnotherCar = true;
+            string vendor;
+            int totalCars = 0;
+            double sumOfCarPrices = 0.0;
+            double salary;
 
-            int quantityPerUser;
-            string continueWithRegistries;
-            
-            Console.WriteLine("Ingrese la cantidad de sets de toallas a comprar");
-            quantityPerUser = Validations.ValidateInteger(Console.ReadLine());
+            Console.WriteLine("Ingrese el nombre del vendedor: ");
+            vendor = Console.ReadLine();
 
-            // Mensaje informativo sobre el envio
-            if (quantityPerUser < 3)
+            // Registro de m carros vendidos para un vendedor m
+            while (registryAnotherCar) //boolean - bandera
             {
-                Console.WriteLine("Se cobrará el envio");
+                double carPrice = 0.00;
+                char continueRegistries = 'S';
+
+                Console.WriteLine("Ingrese el precio del vehiculo vendido: ");
+                carPrice = Validations.ValidateDouble(Console.ReadLine());
+
+                sumOfCarPrices += carPrice;
+                totalCars++;
+
+                Console.WriteLine("Desea seguir registrando autos? (S/N)");
+                continueRegistries = Convert.ToChar(Console.ReadLine());
+
+                // Continuar registrando o no?
+                switch (continueRegistries) //char - centinela
+                {
+                    case 'S':
+                        registryAnotherCar = true;
+                        break;
+                    case 'N':
+                        registryAnotherCar = false;
+                        break;
+                    default:
+                        registryAnotherCar = false;
+                        break;
+                }
             }
-            else
-            {
-                Console.WriteLine("No se cobrará el envio");
-            }
-            
-            totalQuantity += quantityPerUser;
-            
-            Console.WriteLine("Desea ingresar otro pedido? (S/N): ");
-            continueWithRegistries = Console.ReadLine();
-            Console.Clear();
 
-            switch (continueWithRegistries)
-            {
-               case "S":
-                   isCurrentlyWorking = true;
-                   break;
-               case "N":
-                   isCurrentlyWorking = false;
-                   break;
-               default:
-                   isCurrentlyWorking = false;
-                   break;
-            } 
-            
+            salary = SALARY_BASE + (totalCars * BONUS) + (sumOfCarPrices * COMISION);
+
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine($"NOMBRE VENDEDOR : {vendor}");
+            Console.WriteLine($"TOTAL DE AUTOS VENDIDOS : {totalCars}");
+            Console.WriteLine($"SUELDO : $ {salary}");
+            Console.WriteLine("-------------------------------------------------------");
+
+            Console.WriteLine("Hya otro vendedor? (S/N) ");
+            biweekly = Console.ReadLine() == "S"; //true ? //False
         }
-        
-        Console.Clear();
-        Console.WriteLine(" --- Ejercicio de compra de toallas --- ");
-        Console.WriteLine($" Cantidad total de sets toallas vendidas: {totalQuantity} ");
 
+        Console.WriteLine("Fin de la ejecucion...");
+        Console.ReadKey();
     }
 }

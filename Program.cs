5f0ec28cd038bdd3d72e2fa36f65dc66941923cd
Stanley Melgar <stanlydoge@gmail.com>
2025-04-13 19:53:14 -0600

    `tutoria 13:` ejecicio do-while 1

--- Program.cs
+++ Program.cs
@@ -6,67 +6,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        const double SALARY_BASE = 182.50; //365.00
-        const int BONUS = 100;
-        const double COMISION = 0.03; //3%
-        bool biweekly = true;
 
-        // Quincena iniciada
-        // Registro de n vendedores
-        while (biweekly) //boolean - bandera
+        double packageWeight; //peso individuales
+        int totalPackageCount = 0;
+        int lowerToTen = 0;
+        int betweenTenTwenty = 0;
+        int higherToTwenty = 0; //conteo de paquetes
+
+        do
         {
-            bool registryAnotherCar = true;
-            string vendor;
-            int totalCars = 0;
-            double sumOfCarPrices = 0.0;
-            double salary;
+            
+        Console.WriteLine("Ingrese el peso del paquete en kg: ");
+        packageWeight = Validations.ValidateDouble(Console.ReadLine()); //VALIDAR QUE LA ENTRADA DEL USUARIO SEA UN DIGITO NUMERICO
 
-            Console.WriteLine("Ingrese el nombre del vendedor: ");
-            vendor = Console.ReadLine();
+        if (packageWeight != 0)
+        {
+            totalPackageCount++;
 
-            // Registro de m carros vendidos para un vendedor m
-            while (registryAnotherCar) //boolean - bandera
+            if (packageWeight < 10)
             {
-                double carPrice = 0.00;
-                char continueRegistries = 'S';
-
-                Console.WriteLine("Ingrese el precio del vehiculo vendido: ");
-                carPrice = Validations.ValidateDouble(Console.ReadLine());
-
-                sumOfCarPrices += carPrice;
-                totalCars++;
-
-                Console.WriteLine("Desea seguir registrando autos? (S/N)");
-                continueRegistries = Convert.ToChar(Console.ReadLine());
-
-                // Continuar registrando o no?
-                switch (continueRegistries) //char - centinela
-                {
-                    case 'S':
-                        registryAnotherCar = true;
-                        break;
-                    case 'N':
-                        registryAnotherCar = false;
-                        break;
-                    default:
-                        registryAnotherCar = false;
-                        break;
-                }
+                lowerToTen++;
             }
-
-            salary = SALARY_BASE + (totalCars * BONUS) + (sumOfCarPrices * COMISION);
-
-            Console.WriteLine("-------------------------------------------------------");
-            Console.WriteLine($"NOMBRE VENDEDOR : {vendor}");
-            Console.WriteLine($"TOTAL DE AUTOS VENDIDOS : {totalCars}");
-            Console.WriteLine($"SUELDO : $ {salary}");
-            Console.WriteLine("-------------------------------------------------------");
-
-            Console.WriteLine("Hya otro vendedor? (S/N) ");
-            biweekly = Console.ReadLine() == "S"; //true ? //False
+            else if (packageWeight >= 10 && packageWeight < 20)
+            {
+                betweenTenTwenty++;
+            }
+            else
+            {
+                higherToTwenty++;
+            }
         }
-
-        Console.WriteLine("Fin de la ejecucion...");
-        Console.ReadKey();
+        
+        
+        } while (packageWeight != 0);
+        
+        Console.WriteLine($"Paquetes inferiores a 10 kg: {lowerToTen}");
+        Console.WriteLine($"Paquetes entre 10 kg y 20 kg: {betweenTenTwenty}");
+        Console.WriteLine($"Paquetes superiores a 20 kg: {higherToTwenty}");
+        Console.WriteLine($"Paquetes totales pesados: {totalPackageCount}");
+        
+       Console.WriteLine("\n Presione cualquier tecla para continuar...");
+       Console.ReadKey();
     }
 }

94f11ba450e7abd4122ae42b5be2fb1093bbcbf1
Stanley Melgar <stanlydoge@gmail.com>
2025-05-24 21:03:56 -0600

    `tutoria 19:` escribir archivo .csv

--- Program.cs
+++ Program.cs
@@ -5,54 +5,81 @@ using System.Data;
 using System.Data.OleDb;
 using System.Xml.Serialization;
 
-public struct Device
+public struct Movie
 {
-   public string Type { get; set; } 
-   public string Brand { get; set; }
-   public string Model { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public string Gender { get; set; }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<Device> devices = new List<Device>
-        {
-            new Device{ Type="LAPTOP", Brand = "ACER", Model = "AC1010D"},
-            new Device{ Type="ROUTER", Brand = "AZUS", Model = "ROU233T"},
-            new Device{ Type="TABLET", Brand = "SAMSUNG", Model = "SAM123H"},
-            new Device{ Type="CELULAR", Brand = "HUAWEI", Model = "HU1234G"},
-        };
-        
-        //XML
-        XmlSerializer serializer = new XmlSerializer(typeof(List<Device>), new XmlRootAttribute("Devices"));
-        string xmlFilePath = "dispositivosElectronicos.xml";
+        Movie[] movies = new Movie[3];
+        string csvFilePath = "peliculas.csv";
+        string secondCsvFilePath = "idioma.csv";
 
-        //Un espacio seguro para errores
+        //Guardar datos
+        for (int i = 0; i < movies.Length; i++) //declarativas vs imperativas
+        {
+            //Almacenamiento de nombre
+            Console.WriteLine($"Ingresa los datos de la pelicula #{i + 1}:");
+
+            Console.Write("Nombre: ");
+            movies[i].Name = Console.ReadLine();
+            
+            //Almacenamiento de año
+            Console.Write("Año: ");
+            movies[i].Year = Validations.ValidateInteger(Console.ReadLine());
+            
+            //Almacenamiento de género
+            Console.Write("Género: ");
+            movies[i].Gender = Console.ReadLine();
+        }
+        
+        //Escribir archivo
         try
         {
-            using (StreamWriter sw = new StreamWriter(xmlFilePath))
+            using (StreamWriter sw = new StreamWriter(csvFilePath))
             {
-                serializer.Serialize(sw, devices);
-            } 
-            
-            Console.WriteLine("XML file generated successfully");
+                sw.WriteLine("Name,Year,Gender");
+
+                foreach (Movie movie in movies)
+                {
+                    sw.WriteLine($"{movie.Name}, {movie.Year}, {movie.Gender}");
+                }
+            }
+
+            Console.WriteLine($"The file {csvFilePath} was created");
         }
         catch (Exception ex)
         {
-           Console.WriteLine($"Something went wrong: {ex.Message}"); 
+            Console.WriteLine($"Something goes wrong {csvFilePath}: {ex.Message}");
         }
         
-        //Mostrar el contenido en el orden solicitado
-        Console.WriteLine("SHOWING THE CONTENT OF DEVICES");
-        foreach (Device d in devices)
+        //Idiomas
+        Console.WriteLine("Ingrese los idiomas para cada pelicula");
+        try
         {
-            //tipo
-            //marca
-            //modelo
-           Console.WriteLine(d.Type); 
-           Console.WriteLine(d.Brand); 
-           Console.WriteLine(d.Model); 
+            using (StreamWriter sw = new StreamWriter(secondCsvFilePath))
+            {
+               sw.WriteLine("Name,Year,Gender, Language");
+
+               foreach (Movie movie in movies)
+               {
+                   Console.WriteLine($"Para la pelicula {movie.Name}, decime su estado (doblada/con titulos): ");
+                   string languageSelected = Console.ReadLine();
+                   
+                   sw.WriteLine($"{movie.Name}, {movie.Year}, {movie.Gender}, {languageSelected}");
+               }
+            }
+            
+            Console.WriteLine($"The file {secondCsvFilePath} was created");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Something goes wrong {secondCsvFilePath}: {e.Message}");
         }
         
     }
